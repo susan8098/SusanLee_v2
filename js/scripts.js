79c9743ec6d4f9b8eefc1ab6b019d411bb3e7e10
Susan <susan8098@gmail.com>
2020-05-18 22:44:47 -0400

    height

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var tl = gsap.timeline();
+
+// tl.set('.text-block', { opacity: 0 });
+// tl.set('.headshot', { opacity: 0 });
+// tl.set('.color-block', { width: '100%' });
+
+
+// tl.to('.color-block', {
+//   width: '30%',
+//   duration: 3,
+// })
+
+
+// tl.play();
